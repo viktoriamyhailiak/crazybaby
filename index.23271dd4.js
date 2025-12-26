e16b46b061b0fbe3190322462f6266d0c5ce901f
Viktoriia <viktoriamyhailiak@gmail.com>
2025-12-26 21:28:22 +0200

    make build

---
+++ index.23271dd4.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.23271dd4.js.map
---
+++ index.2c964a2a.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.2c964a2a.js.map
